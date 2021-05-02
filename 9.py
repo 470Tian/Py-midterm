e6f2c6de047b13a2e524bf440760b78791257270
unknown <C109156146@nkust.edu.tw>
2021-05-02 17:21:18 +0800

    C109156146

---
+++ 9.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 17:01:34 2020
+
+@author: user
+"""
+
+s1=input("輸入s1為:")
+s2=input("輸入s2為:")
+if s1 in s2:
+    print("YES")
+else:
+    print("NO")
+
+
