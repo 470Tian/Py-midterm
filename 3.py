5ab1ff5c216e9549772b2f005af8765c498b328c
unknown <C109156146@nkust.edu.tw>
2021-05-02 17:09:55 +0800

    C109156146

---
+++ 3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 23:39:20 2020
+
+@author: user
+"""
+n=int(input())
+list1=["monkey","rooster","dog","pig","rat","ox","tiger","rabbit","dragon","snake","horse","sheep"]
+print(list1[n%12])
