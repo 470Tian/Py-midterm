dadbc69839e4f5f6ab96ffeff88aaf8e630517cc
unknown <C109156146@nkust.edu.tw>
2021-05-02 17:25:11 +0800

    C109156146

---
+++ 12.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 16:15:48 2020
+
+@author: user
+"""
+data=input("輸入一整數序列為:").split(" ")
+setdata=set(data)
+test=True
+for i in setdata:     
+    if data.count(i) >= len(data)/2:
+        print("過半元素為:",i)
+        test=False
+        break
+if test:
+    print("過半元素為:","NO")
